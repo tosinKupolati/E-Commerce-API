f47802f4a5741de93a7e772f637aad2246ef297b
Tosin Kupolati <kupolatioluwatosin@gmail.com>
2022-08-25 02:25:05 +0100

    Initial Commit

---
+++ models/temp.js
@@ -0,0 +1,36 @@
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId('62f0c50d8ac7aa3a0ba2b5dc'),
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageRating: {
+        $avg: '$rating',
+      },
+      numOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
+
+MongoClient.connect(
+  '',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (connectErr, client) {
+    assert.equal(null, connectErr);
+    const coll = client.db('').collection('');
+    coll.aggregate(agg, (cmdErr, result) => {
+      assert.equal(null, cmdErr);
+    });
+    client.close();
+  }
+);
